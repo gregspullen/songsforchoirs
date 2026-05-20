bdb821b705d95b86327c9b4f869878fa5dfa7c91
gregspullen <gregpullen21@googlemail.com>
2026-05-20 09:06:34 +0100

    Update data.js

--- data.js
+++ data.js
@@ -16,7 +16,7 @@ window.PIECES = {
 },
   "songs-of-advent": {
   "title": "Songs of Advent",
-  "description": "Songs of Thanksgiving - joyful and rhythmic choral settings for SATB choir, written in a similar styles. Energetic, engaging, and accessible. View the score, listen, and download licensed copies from Songs for Choirs.",    
+  "description": "ongs of Advent - joyful and atmospheric choral settings for mixed choirs. Energetic, engaging, and accessible. View the score, listen, and download licensed copies from Songs for Choirs.",    
   "voicing": "Mixed Choirs",
   "accompaniment": "piano",
   "categories": ["albums"],
